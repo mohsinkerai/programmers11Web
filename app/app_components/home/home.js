ada8f516a8194f5959d71b5f98ae1423fdd6bef1
Mohsin Mansoor Kerai <mohsin.kerai@gmail.com>
2015-12-27 17:01:14 +0500

    Integrated Portfolio

--- app/app_components/home/home.js
+++ app/app_components/home/home.js
@@ -11,6 +11,10 @@ angular.module('home',['home.routes'])
   .controller('HomeCtrl', function ($scope) {
     $scope.mainContent = 'Main Screen';
 
+    $scope.web = true;
+    $scope.mob = true;
+    $scope.site = true;
+
     var pattern = Trianglify({
       height: 1080,
       width: 1920,
@@ -25,4 +29,24 @@ angular.module('home',['home.routes'])
       "background-image": "url("+imgURL+")"
     }
     angular.element("#canvasDiv").append(pattern.canvas());
+
+    $scope.showPortfolio = function(portfolioName){
+      if(portfolioName === 'All'){
+        $scope.web = true;
+        $scope.app = true;
+        $scope.site = true;
+      }else if(portfolioName === 'Web'){
+        $scope.web = true;
+        $scope.app = false;
+        $scope.site = false;
+      }else if(portfolioName === 'App'){
+        $scope.web = false;
+        $scope.app = true;
+        $scope.site = false;
+      }else if(portfolioName === 'Site'){
+        $scope.web = false;
+        $scope.app = false;
+        $scope.site = true;
+      }
+    }
   });
--- app/app_components/home/homeRoutes.js
+++ app/app_components/home/homeRoutes.js
@@ -7,6 +7,16 @@ angular.module('home.routes',['ui.router']).config(function($stateProvider){
         "home": {
           templateUrl: "app_components/home/home.html",
           controller: 'HomeCtrl'
+        },
+        "services": {
+          templateUrl: "app_components/services/services.html"
+        },
+        "aboutus": {
+          templateUrl: "app_components/aboutus/aboutus.html"
+        },
+        "portfolio": {
+          templateUrl: "app_components/portfolio/portfolio.html",
+          controller: 'HomeCtrl'
         }
       },
       controller: 'HomeCtrl'
